0896472599ce0523e18fc9ca6cf35e7042693b93
Leonardo Bartoloni <leonardo@zavfit.com>
2023-12-28 14:34:57 +0100

    Remove unnecessary NgModule classes
    ng g @angular/core:standalone

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,18 +3,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
     imports: [
-        IonicModule,
-        CommonModule,
-        FormsModule,
-        ExploreContainerComponentModule,
-        Tab1PageRoutingModule,
-        Tab1Page
-    ]
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    Tab1PageRoutingModule,
+    Tab1Page
+]
 })
 export class Tab1PageModule {}
